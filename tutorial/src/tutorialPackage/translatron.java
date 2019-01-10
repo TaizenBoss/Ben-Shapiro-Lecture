29b8524d817326b0363297fac66bee0d8b5c19f7
giovannir1 <giovannir1@091DS-1182-25.student.cssd.ab.ca>
2019-01-10 13:52:12 -0700

    translator

--- tutorial/src/tutorialPackage/translatron.java
+++ tutorial/src/tutorialPackage/translatron.java
@@ -1,13 +1,80 @@
 package tutorialPackage;
-
+import java.util.Scanner;
 public class translatron {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner word = new Scanner(System.in);
+		Scanner nums = new Scanner(System.in);
+		int stay;
 		String[] englishWords = {"friend",
 								"basketball",
-								""
-		}; 
-	}
+								"steal",
+								"group of friends",
+								"poor neighbourhood",
+								"prepare to fight",
+								"money",
+								"excellent",
+								"abondoned house",
+								"gun",
+								"about to",
+								"air jordans",
+								"Toronto",
+								"advocating for",
+								"asking a girl out"};
+		
+		
+		String[] hoodWords = {"homie",
+				"rock",
+				"chored",
+				"gang",
+				"hood",
+				"square up",
+				"bread",
+				"fire",
+				"bando",
+				"piece",
+				"finna",
+				"j's",
+				"the 6",
+				"reppin'",
+				"shooting your shot"
+				
+		};
+		
+		for(int i = 0; i < hoodWords.length; i++) {
+			if(i == 0) {
+			System.out.println("English Words in Translation Index: ");
+			}
+			System.out.println(englishWords[i] + "	");
+		
+		}
+		System.out.print("\n\nWould you like to start translating, user?\nEnter 1 to start translating");
+		stay = nums.nextInt();
+		while(stay == 1) {
+			System.out.println("\n\n");
+			System.out.println("What word would you like translated into hood language, user?\nPlease enter your word here:");
+			String engword  = word.nextLine();
+		
+		
+			for(int i = 0; i < hoodWords.length; i++) {
+				if(engword.equalsIgnoreCase(englishWords[i])) {
+					System.out.println("The hood version of the word: " + hoodWords[i]);
+					break;}
+				else if(!engword.equalsIgnoreCase(englishWords[i]) && i == hoodWords.length-1) {
+					System.out.println("Um, sorry. We don't have that word in our index. Please reread the list of words at the top and try again.\nEnter here: ");
+					word.nextLine();
+					String engword2 = word.nextLine();
+					if(engword2.equalsIgnoreCase(englishWords[i])) {
+						System.out.println("The hood version of the word: " + hoodWords[i]);
+						break;}
+				}
+				}
+			System.out.println("Would you like to continue,  user?\nEnter 1 to continue translating.");
+			stay = nums.nextInt();
+			}//end while		
+			System.out.print("Bye, user!");
+		
+	}//end main
 
-}
+}//end class
